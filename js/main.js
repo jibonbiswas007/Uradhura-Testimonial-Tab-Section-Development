27c8c943a94d29d25bd20438b8d35bdadd3bd36a
jibonbiswas007 <33145139+jibonbiswas007@users.noreply.github.com>
2017-12-08 03:10:10 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(function($) {
+    "use strict";
+    
+    jQuery(document).ready(function($){
+        
+        $(".test-athor-name ul").perfectScrollbar();
+        
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+    
+    
+}(jQuery));
